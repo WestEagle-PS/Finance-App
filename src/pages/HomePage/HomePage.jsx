bbdede17f5ee62bed60ae0e8c568478da0a45324
Zhvanko Anna <zhvankoann@gmail.com>
2023-03-15 21:09:42 +0200

    Fix add button

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Modal from 'shared/components/Modal/Modal';
-import Button from 'shared/components/Button/Button';
 import AddTransactionForm from 'components/AddTransactionForm/AddTransactionForm';
 import css from './home-page.module.scss';
 import Header from 'components/Header/Header';
@@ -10,7 +9,6 @@ const HomePage = () => {
   const [showModal, setShowModal] = useState(false);
 
   const handleAddBtnClick = () => {
-    console.log('12356')
     setShowModal(true);
   }
 
@@ -22,7 +20,8 @@ const HomePage = () => {
     <div className="container">
       <Header />
       <h2 className={css.title}>Hello! It`s Home page</h2>
-      <Button onClick={handleAddBtnClick}>+</Button>
+      <Currency/>
+      <button type="button" onClick={handleAddBtnClick}>+</button>
       {showModal && <Modal onClose={onCloseModal}><AddTransactionForm/></Modal>}
     </div>
   );
