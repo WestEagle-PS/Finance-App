0adbd417def1ccb9e63589d780fe871c440a29b4
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-03-15 19:31:14 +0200

    updated markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import UserRoutes from 'UserRoutes';
 import { BrowserRouter } from 'react-router-dom';
-import Header from 'components/Header/Header';
 
 import './shared/styles/styles.scss';
 
 export const App = () => {
   return (
     <BrowserRouter basename="/wallet">
-      <Header />
       <UserRoutes />
     </BrowserRouter>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,13 @@
 import css from './home-page.module.scss';
+import Header from 'components/Header/Header';
+import Currency from 'components/Currency/Currency'
 
 const HomePage = () => {
   return (
     <div className="container">
+      <Header />
       <h2 className={css.title}>Hello! It`s Home page</h2>
+      <Currency />
     </div>
   );
 };
