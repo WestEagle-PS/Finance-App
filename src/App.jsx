368c6e5f67e26a0b4d520fb411d695f5ac29cfcc
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-03-15 15:54:04 +0200

    updated header

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 // import { PersistGate } from 'redux-persist/integration/react';
 // import { store, persistor } from 'redux/store';
 import UserRoutes from './UserRoutes';
+import Header from 'components/Header/Header';
 
 import './shared/styles/styles.scss';
 
 export const App = () => {
   return (
     <BrowserRouter basename="/wallet">
+      <Header />
       <UserRoutes />
     </BrowserRouter>
   );
