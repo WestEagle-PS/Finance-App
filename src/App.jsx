0282fb62170d78717cb1a3a9d8d7c2fe983dfae2
Anton Orlov <anton_orlov@ukr.net>
2023-03-15 19:38:02 +0200

    delete import Header

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { store, persistor } from 'redux/store';
 import UserRoutes from 'UserRoutes';
 
 import AuthLayout from './modules/AuthLayout/AuthLayout';
-import Header from 'components/Header/Header';
 
 import './shared/styles/styles.scss';
 
