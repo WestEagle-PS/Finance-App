8d57646babf5729543003202d4157c491d9385e3
Your Name <yaremiy.nazar@gmail.com>
2023-03-16 10:36:47 +0200

    YN_logout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { getUser } from '../../redux/auth/auth-selectors';
+import { useState } from 'react';
 import { logout } from '../../redux/auth/auth-operations';
+import Modal from 'shared/components/Modal/Modal';
 
 import logo from '../../images/svg/Group.png';
 import vector from '../../images/svg/Vector.svg';
@@ -9,9 +11,13 @@ import exit from '../../images/svg/exit.svg';
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
   const user = useSelector(getUser);
-
   const dispatch = useDispatch();
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
 
   const onLogout = () => {
     dispatch(logout());
@@ -27,10 +33,26 @@ const Header = () => {
         <div className={styles.exitBox}>
           <p className={styles.name}>{user.username}</p>
           <img className={styles.vector} src={vector} height="30" alt="" />
-          <img onClick={onLogout} className={styles.exit} src={exit} alt="" />
-          <button onClick={onLogout} className={styles.button} type="button">
+          <img onClick={toggleModal} className={styles.exit} src={exit} alt="" />
+          <button onClick={toggleModal} className={styles.button} type="button">
             Exit
           </button>
+          {showModal && (
+            <Modal onClose={toggleModal}>
+              <div className={styles.modal__wrapper}>
+                <p className={styles.question}> Are you sure you want to exit ?</p>
+                <div className={styles.button__wrapper}>
+                  <button className={styles.button__question} onClick={onLogout}>
+                    yes
+                  </button>
+
+                  <button className={styles.button__question} onClick={toggleModal}>
+                    no
+                  </button>
+                </div>
+              </div>
+            </Modal>
+          )}
         </div>
       </div>
     </header>
