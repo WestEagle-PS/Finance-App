3698e36af5e404cdbc7bc98ad14fa431cb60b4c8
Anton Orlov <anton_orlov@ukr.net>
2023-03-16 16:27:26 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -6,12 +6,12 @@ import Modal from 'shared/components/Modal/Modal';
 import AddTransactionForm from 'components/AddTransactionForm/AddTransactionForm';
 import css from './home-page.module.scss';
 import Header from 'components/Header/Header';
-import Currency from 'components/DashBoard/SideBar/Currency/Currency';
+// import Currency from 'components/DashBoard/SideBar/Currency/Currency';
 import ChartStat from 'components/DashBoard/ChartStat/ChartStat';
-import PieChartComponent from 'components/DashBoard/ChartDiagram/ChartDiagram';
-import NavigationDashboard from 'components/DashBoard/SideBar/NavigationDashboard/NavigationDashboard';
-import Balance from 'components/DashBoard/SideBar/Balance/Balance';
-import SideBar from 'components/DashBoard/SideBar/SideBar';
+// import PieChartComponent from 'components/DashBoard/ChartDiagram/ChartDiagram';
+// import NavigationDashboard from 'components/DashBoard/SideBar/NavigationDashboard/NavigationDashboard';
+// import Balance from 'components/DashBoard/SideBar/Balance/Balance';
+// import SideBar from 'components/DashBoard/SideBar/SideBar';
 import DashBoard from 'components/DashBoard/DashBoard';
 
 const HomePage = () => {
