3a9f78c8d937393fa80219747befd1d5341503c1
Anton Orlov <anton_orlov@ukr.net>
2023-03-16 17:03:50 +0200

    update structure pages

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,15 +4,7 @@ import { getAllCategories } from 'redux/transaction/transaction-operations';
 import AddButton from 'shared/components/AddButton/ButtonPlus';
 import Modal from 'shared/components/Modal/Modal';
 import AddTransactionForm from 'components/AddTransactionForm/AddTransactionForm';
-import css from './home-page.module.scss';
-import Header from 'components/Header/Header';
-import Currency from 'components/DashBoard/SideBar/Currency/Currency';
-import ChartStat from 'components/DashBoard/ChartStat/ChartStat';
-import PieChartComponent from 'components/DashBoard/ChartDiagram/ChartDiagram';
-import NavigationDashboard from 'components/DashBoard/SideBar/NavigationDashboard/NavigationDashboard';
-import Balance from 'components/DashBoard/SideBar/Balance/Balance';
-import SideBar from 'components/DashBoard/SideBar/SideBar';
-import DashBoard from 'components/DashBoard/DashBoard';
+// import css from './home-page.module.scss';
 
 const HomePage = () => {
   const [showModal, setShowModal] = useState(false);
@@ -36,12 +28,8 @@ const HomePage = () => {
   };
 
   return (
-    <div className="container">
-      <Header />
-      <h2 className={css.title}>Hello! It`s Home page</h2>
-      <DashBoard />
-      <ChartStat />
-      {/* <PieChartComponent /> */}
+    <div>
+      <p>Home page</p>
       <AddButton type="button" onBtnClick={handleAddBtnClick} />
       {showModal && (
         <Modal onClose={onCloseModal}>
