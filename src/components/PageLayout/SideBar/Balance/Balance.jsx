3a25bc12b4310aaa92c7e7e2c0fec1f67687772f
Ihor <ihorfront88@gmail.com>
2023-03-18 14:24:57 +0200

    add balance

--- src/components/PageLayout/SideBar/Balance/Balance.jsx
+++ src/components/PageLayout/SideBar/Balance/Balance.jsx
@@ -1,10 +1,15 @@
+import { useSelector } from 'react-redux';
+import { selectAllTransactions } from 'redux/transaction/transaction-selectors';
 import s from './Balance.module.scss';
 
 const Balance = () => {
+  const data = useSelector(selectAllTransactions);
+  const totalBalance = data.reduce((_, el) => el.balanceAfter, 0);
+
   return (
     <div className={s.balance}>
       <div className={s.balanceTitle}>Your balance</div>
-      <div className={s.balanceValue}>₴ 24 000.00</div>
+      <div className={s.balanceValue}>&#8372; {totalBalance > 0 ? totalBalance : 0}</div>
     </div>
   );
 };
