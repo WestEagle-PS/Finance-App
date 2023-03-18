38a02dcf3fdac8a64752b57b4dad8a2f9dece421
Vitalii Koshytskyi <115081482+VitaliiKoshytskyi@users.noreply.github.com>
2023-03-18 19:46:39 +0200

    update markup

--- src/UserRoutes.js
+++ src/UserRoutes.js
@@ -9,6 +9,7 @@ const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
 const StatisticPage = lazy(() => import('./pages/StatisticPage/StatisticPage'));
 const RegisterPage = lazy(() => import('./pages/RegisterPage/RegisterPage'));
 const LoginPage = lazy(() => import('./pages/LoginPage/LoginPage'));
+const Currency = lazy(() => import('./pages/CurrencyPage/CurrencyPage'));
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 const UserRoutes = () => {
@@ -20,6 +21,7 @@ const UserRoutes = () => {
             <Route path="/" element={<LoginPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/currency" element={<Currency />} />
           </Route>
 
           <Route element={<PrivateRoute />}>
--- src/pages/StatisticPage/StatisticPage.jsx
+++ src/pages/StatisticPage/StatisticPage.jsx
@@ -1,14 +1,13 @@
 import PieChartComponent from 'components/PageLayout/ChartDiagram/ChartDiagram';
 import MonthCalendar from 'components/MonthCalendar/MonthCalendar';
 import YearsCalendar from 'components/YearsCalendar/YearsCalendar';
-// import ChartStat from 'components/PageLayout/ChartStat/ChartStat';
+
 import css from './statistic-page.module.scss';
 import ExpensesList from 'components/ExpensesList/ExpensesList';
 
 const StatisticPage = () => {
   return (
     <div className={css.wrapper}>
-      {/* <ChartStat /> */}
       <PieChartComponent />
       <div className={css.box}>
         <div className={css.innerBox}>
