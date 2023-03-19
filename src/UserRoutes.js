20e93f7c777fb831bce8b16d8293257d447a0374
Ihor <ihorfront88@gmail.com>
2023-03-19 12:02:35 +0200

    add: Route to CurrencyPage to mobile adaptive

--- src/UserRoutes.js
+++ src/UserRoutes.js
@@ -1,6 +1,6 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Loader from './shared/components/Loader/Loader'
+import Loader from './shared/components/Loader/Loader';
 
 import PrivateRoute from './modules/PrivateRoute/PrivateRoute';
 import PublicRoute from './modules/PublicRoute/PublicRoute';
@@ -15,18 +15,18 @@ const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 const UserRoutes = () => {
   return (
     <>
-      <Suspense fallback={<Loader/>}>
+      <Suspense fallback={<Loader />}>
         <Routes>
           <Route element={<PublicRoute />}>
             <Route path="/" element={<LoginPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="/currency" element={<Currency />} />
           </Route>
 
           <Route element={<PrivateRoute />}>
             <Route path="/home" element={<HomePage />} />
             <Route path="/statistic" element={<StatisticPage />} />
+            <Route path="/currency" element={<Currency />} />
           </Route>
 
           <Route path="*" element={<NotFoundPage />} />
